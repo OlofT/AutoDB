35af52e9fbe4ba04e64a6c3df2d59609982c6a34
Olof Thorén <olof.thoren@qvik.fi>
2021-07-08 13:12:37 +0200

    Testing Xcode Cloud

---
+++ AutoDBSwift/Shared/AutoDBSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoDBSwiftApp.swift
+//  Shared
+//
+//  Created by Olof Thorén on 2021-07-08.
+//
+
+import SwiftUI
+
+@main
+struct AutoDBSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
